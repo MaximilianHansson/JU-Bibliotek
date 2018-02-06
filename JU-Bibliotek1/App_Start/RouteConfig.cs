21c6902f6edeff6f309121f3fa3600dbad45f351
HaMa1654 <hama1654@student.ju.se>
2018-02-06 09:18:09 +0100

    Included route and controller

--- JU-Bibliotek1/App_Start/RouteConfig.cs
+++ JU-Bibliotek1/App_Start/RouteConfig.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-//This is a test
+
 namespace JU_Bibliotek1
 {
     public class RouteConfig
@@ -12,11 +12,11 @@ namespace JU_Bibliotek1
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            //This is test nr 2
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "index", id = UrlParameter.Optional }
             );
         }
     }
